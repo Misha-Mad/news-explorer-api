d25acac12388c8ded57135754545ce35089a87c0
Mikhail Zakharov <zakharov3010@yandex.ru>
2020-12-06 17:49:34 +0300

    fix mongo path

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 const JWT_SECRET_DEV = '3a2d3974c37b4229658e732f0e90b19560a4a3b40285b76132337a2cdafb5577';
-const DB_CONN_DEV = 'mongodb://127.0.0.1:27017/newsexplorerdb';
+const DB_CONN_DEV = 'mongodb://localhost:27017/newsexplorerdb';
 
 module.exports = {
   JWT_SECRET_DEV,
